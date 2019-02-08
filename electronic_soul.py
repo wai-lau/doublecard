f6fe79a56b5df582c0ea807c671e6eeaa3fc4045
Wai-Lau <wl.wailau@gmail.com>
2019-02-07 21:18:49 -0500

    Added chaos_naive

--- electronic_soul.py
+++ electronic_soul.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle, random
 from move_finder import MoveFinder
 
 class ElectronicSoul:
@@ -6,11 +6,13 @@ class ElectronicSoul:
         self.mf = MoveFinder()
         self.bs = bs
         self.baz = baz
-        self.token = 'dots'
+        self.token = token
         if method == "naive_single_layer":
             self.method = self.naive_single_layer
         elif method == "chaos_monkey":
             self.method = self.chaos_monkey
+        elif method == "chaos_naive":
+            self.method = self.chaos_naive
 
     def get_move(self, board):
         return self.method(board)
@@ -27,10 +29,19 @@ class ElectronicSoul:
             if (h - current) > best_diff:
                 best_move = m
                 best_diff = h - current
-
         return best_move
 
     def chaos_monkey(self, board):
         moves = self.mf.find_moves(board)
         shuffle(moves)
         return moves[0]
+
+    def chaos_naive(self, board):
+        if all(not c[0] for c in board):
+            return self.chaos_monkey(board)
+        if random()*100 < 33:
+            print("CHAOS PLAYS")
+            return self.chaos_monkey(board)
+        else:
+            print("-naive- plays")
+            return self.naive_single_layer(board)
