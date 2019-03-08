affe774f12879c110ac3f0169f503807c7ff3d82
Amanda <amawai@hotmail.com>
2019-03-07 21:45:29 -0500

    Souls can now WRITE TO FILES

--- useless_soul.py
+++ useless_soul.py
@@ -2,10 +2,10 @@ from electronic_soul import ElectronicSoul
 
 class UselessSoul(ElectronicSoul):
     def move(self, board, token, moves_played_count, *args):
-        return self.m_search(board, token, 0, 2, moves_played_count)[0]
+        return self.m_search(board, token, 0, 3, moves_played_count)[0]
 
     def recycle(self, board, token, last_move, *args):
-        return self.m_cycle(board, token, 0, 2, last_move)[0]
+        return self.m_cycle(board, token, 0, 3, last_move)[0]
 
     def m_search(self, board, token, depth, max_depth, moves_played_count):
         possible_moves = self.mf.find_moves(board)
@@ -32,6 +32,8 @@ class UselessSoul(ElectronicSoul):
                 if h*-1 > best_score:
                     best_score = h*-1
                     best_move = m
+        if depth == 2:
+            self.append_to_file('output.txt', str(best_score) + '\n')
         return best_move, best_score
 
     def m_cycle(self, board, token, depth, max_depth, last_move):
@@ -60,3 +62,11 @@ class UselessSoul(ElectronicSoul):
     def flipside(self, token):
         return "dots" if token == "colors" else "colors"
 
+    def append_to_file(self, filename, data):
+        file = None
+        try:
+            file = open(filename,'a')
+        except:
+            # if the file doesn't exist, create it
+            file = open(filename,'w')
+        file.write(data)
