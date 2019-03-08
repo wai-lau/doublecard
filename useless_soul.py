97e77e0e4aa5b05c86b2974427c3a437fd0d9f82
Jad Malek <jadmalek9@hotmail.com>
2019-03-07 23:54:43 -0500

    Add class variable/tracing of depth 2 values

--- useless_soul.py
+++ useless_soul.py
@@ -5,6 +5,7 @@ class UselessSoul(ElectronicSoul):
     def __init__(self, bs, baz, mf, generate_trace, sanity=100):
         super().__init__(bs, baz, mf, sanity)
         self.max_depth_heuristic_count = 0
+        self.level_2_node_values = []
         self.generate_trace = generate_trace
         if generate_trace:
             self.create_file(DEFAULT_OUTPUT_FILE)
@@ -14,6 +15,13 @@ class UselessSoul(ElectronicSoul):
             best_move, best_score = self.m_search(board, token, 0, 2, moves_played_count)
             self.append_to_file(DEFAULT_OUTPUT_FILE, str(self.max_depth_heuristic_count))
             self.append_to_file(DEFAULT_OUTPUT_FILE, str(best_score))
+            if self.level_2_node_values:
+                self.append_to_file(DEFAULT_OUTPUT_FILE, "")
+                for node_value in self.level_2_node_values:
+                    self.append_to_file(DEFAULT_OUTPUT_FILE, node_value)
+                self.append_to_file(DEFAULT_OUTPUT_FILE, "")
+            # Clear the list of node-level 2 values for the next search 
+            self.level_2_node_values.clear()
             return best_move
         return self.m_search(board, token, 0, 2, moves_played_count)[0]
 
@@ -51,6 +59,10 @@ class UselessSoul(ElectronicSoul):
                 if h*-1 > best_score:
                     best_score = h*-1
                     best_move = m
+
+                # Filling up the list of e(n) values coming up from level 2
+                if max_depth == 2:
+                    self.level_2_node_values.append(str(best_score))
         return best_move, best_score
 
     def m_cycle(self, board, token, depth, max_depth, last_move):
